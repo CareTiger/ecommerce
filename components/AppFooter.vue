056537a7075403889b200ae537034ccca73f410d
venkateshvadlamani <venkatesh.caretiger@gmail.com>
2019-11-10 05:43:05 -0600

    updates

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <section>
+        <section class="callout w-100 white">
             <h2>"surprising styles of Skyline Ivy are advanced for all seasons."</h2>
             <p>Hansel Andersen</p>
         </section>
@@ -21,5 +21,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.callout {
+    background: url("/callout.jpg") center center no-repeat;
+    background-size: cover;
+}
 
 </style>
